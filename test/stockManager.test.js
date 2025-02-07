2c75527ab26631001286ad98ae2b22fc577ee6b6
antoinecaby <acaby@yipikay.fr>
2025-02-07 11:50:26 +0100

    Premiers test pour l'ajout d'articles

---
+++ test/stockManager.test.js
@@ -0,0 +1,19 @@
+const { addStock, getStock } = require("../src/stockManager");
+
+describe("Ajout d'article", () => {
+  test("Ajout d'un nombre valide d'article existant", () => {
+    const stock = { "Chaise": 5 };
+    addStock(stock, "Chaise", 3);
+    expect(getStock(stock, "Chaise")).toBe(8);
+  });
+
+  test("Ajout d'un nombre négalif d'article existant doit retourner une erreur", () => {
+    const stock = { "Chaise": 5 };
+    expect(() => addStock(stock, "Chaise", -3)).toThrow("Quantité invalide");
+  });
+
+  test("Tenter d'ajouter un article inexistant doit générer une erreur", () => {
+    const stock = { "Chaise": 5 };
+    expect(() => addStock(stock, "Table", 3)).toThrow("Article inexistant");
+  });
+});
